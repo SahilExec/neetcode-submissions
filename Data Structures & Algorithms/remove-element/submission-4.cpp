4866c6bddb3c14f226469874e5a3d3a6e1edd809
Edgex <sahil.exec@gmail.com>
2026-09-16 20:38:04 +0530

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int st = 0;
+        int lst = n-1;
+        while(st<=lst){
+            while(st <= lst && nums[lst] == val ){
+                lst--;
+            }
+
+            if(st <= lst && nums[st] == val){
+                swap(nums[st], nums[lst]);
+                lst--;
+            }
+            else{
+                st++;
+            }
+        }
+
+        return lst+1;
+    }
+};
